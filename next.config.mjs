7596565bfbfec8c94bf887edfefd7be600b9951b
cifelse <meetcif@gmail.com>
2024-03-16 18:31:19 +0800

    Add Next.js PWA files

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+/** @type {import('next').NextConfig} */
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  }
+});
+
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+};
+
+export default withPWA(nextConfig);
