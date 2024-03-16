ebf1ec78e68cd5f45ee5a2d75a1665a731f0f732
cifelse <meetcif@gmail.com>
2024-03-16 21:08:59 +0800

    Add theme-color as a meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="theme-color" content="#317EFB"/>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
