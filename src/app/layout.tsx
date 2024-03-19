d2ba3dfe803083896ba9d2fca35a2471905ac648
cifelse <meetcif@gmail.com>
2024-03-19 17:13:24 +0800

    Add Verifeye metadata and update image paths

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,29 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
   title: "TikTok - Make Your Day",
   description: "TikTok - trends start here. On a device or on the web, viewers can watch and discover millions of personalized short videos. Download the app to get started.",
+  applicationName: "Verifeye",
+  generator: "Next.js",
+  keywords: ["TikTok", "Reels", "Shorts", "Credibility", "Verifeye", "Verify", "Philippines"],
+  openGraph: {
+    images: {
+      url: "https://i.imgur.com/lSEwsnI.png",
+      type: "image/png",
+      width: 1200,
+      height: 630,
+    },
+    type: "website",
+    siteName: "Verifeye",
+    url: "https://comet.dlsu.edu.ph/verifeye/",
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: {
+      url: "https://i.imgur.com/lSEwsnI.png",
+      type: "image/png",
+      width: 1200,
+      height: 630,
+    },
+  },
 };
 
 export default function RootLayout({
