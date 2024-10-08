df6202b3f883a9495d5d9e33d017657dea3223ad
cifelse <meetcif@gmail.com>
2024-10-08 18:53:52 +0800

    Add necessary PWA files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withSerwistInit from "@serwist/next";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Your Next.js configuration here
+};
+
+/** DO NOT TOUCH */
+const withSerwist = withSerwistInit({
+  swSrc: "src/sw.ts",
+  swDest: "public/sw.js",
+});
+
+export default withSerwist(nextConfig);
